138ca6eac3bc08de573a624c450cead5978485cf
robbertploeg@gmail.com <robbertploeg@gmail.com@b613f034-4b44-8a1c-17f4-f9f2dc0a3813>
2013-06-24 09:57:42 +0000

    started work on Confirm Reg Topic

--- trunk/ jms-rrs --username robbertploeg@gmail.com/src/main/java/nl/han/dare2date/service/jms/util/Queues.java
+++ trunk/ jms-rrs --username robbertploeg@gmail.com/src/main/java/nl/han/dare2date/service/jms/util/Queues.java
@@ -2,4 +2,5 @@ package nl.han.dare2date.service.jms.util;
 
 public interface Queues {
 	public final static String EXAMPLE_QUEUE = "ExampleQueue";
+    public final static String CONFIRM_QUEUE = "ConfirmQueue";
 }
--- trunk/ jms-rrs --username robbertploeg@gmail.com/src/main/java/nl/han/dare2date/service/web/ConfirmRegistrationService.java
+++ trunk/ jms-rrs --username robbertploeg@gmail.com/src/main/java/nl/han/dare2date/service/web/ConfirmRegistrationService.java
@@ -1,15 +1,21 @@
 package nl.han.dare2date.service.web;
 
 import nl.han.dare2date.applyregistrationservice.Registration;
+import nl.han.dare2date.service.jms.util.JMSUtil;
+import nl.han.dare2date.service.jms.util.Queues;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
 
 /**
- * 
  * @author mdkr
- *
- * Is used as a JMS publisher
+ *         <p/>
+ *         Is used as a JMS publisher
  */
 public class ConfirmRegistrationService {
-	public void confirm(Registration reg) {
+    public void confirm(Registration reg) {
+        Connection conn = JMSUtil.getConnection();
+        Destination confirmationQueue = JMSUtil.getDestinationForQueue(Queues.CONFIRM_QUEUE);
 
-	}
+    }
 }
