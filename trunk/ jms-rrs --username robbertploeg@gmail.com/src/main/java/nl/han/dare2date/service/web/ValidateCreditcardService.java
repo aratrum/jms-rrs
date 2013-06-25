63f626fd37e52767160a4441f6da1b2e56547d9b
heijmansroy@gmail.com <heijmansroy@gmail.com@b613f034-4b44-8a1c-17f4-f9f2dc0a3813>
2013-06-25 08:44:52 +0000

    Generated files zijn serialisable

--- trunk/ jms-rrs --username robbertploeg@gmail.com/src/main/java/nl/han/dare2date/service/web/ValidateCreditcardService.java
+++ trunk/ jms-rrs --username robbertploeg@gmail.com/src/main/java/nl/han/dare2date/service/web/ValidateCreditcardService.java
@@ -15,22 +15,40 @@ import javax.naming.NamingException;
  */
 public class ValidateCreditcardService {
 	public boolean validate(Creditcard cc) {
+
+        System.out.println("inside cc 1");
         Connection conn = JMSUtil.getConnection();
 
+        System.out.println(conn.toString());
+        System.out.println("inside cc 2");
         String requestQueueName = Queues.REQUEST_QUEUE;
         String replyQueueName = Queues.REPLY_QUEUE;
         String invalidQueueName = Queues.INVALID_QUEUE;
 
+        System.out.println("inside cc 3");
         Creditcard card = new Creditcard();
         card.setCvc(cc.getCvc());
         card.setNumber(cc.getNumber());
         card.setValidThrough(cc.getValidThrough());
 
+        System.out.println("inside cc 4");
         try{
+            System.out.println("inside cc 5");
+            System.out.println(card.getNumber());
+            System.out.println(card.getCvc());
+            System.out.println(card.getValidThrough());
+            System.out.println(requestQueueName);
+            System.out.println(replyQueueName);
+            System.out.println(invalidQueueName);
             ValidateCreditcardRequestor requestor = new ValidateCreditcardRequestor(conn, requestQueueName, replyQueueName, invalidQueueName, card);
+            System.out.println(requestor);
+
+            System.out.println("inside cc 5.5");
             requestor.send();
+            System.out.println("inside cc 6");
             requestor.receiveSync();
             ValidateCreditcardResponse response = requestor.getResponse();
+            System.out.println("inside cc 7");
             conn.close();
             return response.isValid();
         } catch(JMSException e){
